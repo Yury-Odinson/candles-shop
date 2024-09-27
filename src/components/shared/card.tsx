6a7e8ea17702711af1c4a2e10df336a4c9a12ab2
Yury Odintsov <yury.odinson@gmail.com>
2024-09-27 14:29:07 +0300

    feat: link in the card

--- src/components/shared/card.tsx
+++ src/components/shared/card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {CirclePlus} from "lucide-react";
 import {Button} from "@/components/ui";
+import Link from "next/link";
 
 interface Props {
     name: string,
@@ -14,11 +15,13 @@ export const Card: React.FC<Props> = ({name, description, imageURL, id, price}) 
     return (
         <article className="flex flex-col justify-between bg-secondary rounded-2xl" id={`card_${id}`}>
 
-            <div>
-                <p className="p-4 text-2xl font-bold">{name}</p>
-                <img src={imageURL} alt={name}/>
-                <p className="mt-2 p-2 text-justify indent-5">{description}</p>
-            </div>
+            <Link href={"/product/" + id}>
+                <div>
+                    <p className="p-4 text-2xl font-bold">{name}</p>
+                    <img src={imageURL} alt={name}/>
+                    <p className="mt-2 p-2 text-justify indent-5">{description}</p>
+                </div>
+            </Link>
 
             <div className="flex mt-5 justify-between">
                 <p className="text-2xl font-bold pl-4">{price} р.</p>
