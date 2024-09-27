9f5208d4ea962ae8d4f9bea88d40d2d90b5692ce
Yury Odintsov <yury.odinson@gmail.com>
2024-09-27 13:49:17 +0300

    fix: skeleton styles

--- src/components/shared/skeleton-recommended.tsx
+++ src/components/shared/skeleton-recommended.tsx
@@ -5,7 +5,7 @@ export const SkeletonRecommended =() => {
     return (
         <article className="p-2 flex flex-col justify-between min-w-[200px] w-[15vw] h-[35vh] shadow-2xl rounded-2xl">
             <Skeleton className="w-1/2 h-6"/>
-            <Skeleton className="h-[180px] w-[180px] rounded-xl"/>
+            <Skeleton className="min-w-[180px] w-[14vw] h-[25vh] rounded-xl"/>
             <Skeleton className="ml-auto w-1/2 h-6"/>
         </article>
     );
