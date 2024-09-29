7b0dfb83c8cfb41424ae7b2c0a6c0a31f405cdd8
Yury Odintsov <yury.odinson@gmail.com>
2024-09-29 14:27:08 +0300

    fix: top bar styles

--- src/components/shared/top-bar.tsx
+++ src/components/shared/top-bar.tsx
@@ -18,18 +18,20 @@ const categories: object[] = [
 export const TopBar: React.FC<Props> = ({className}) => {
     const categoryActiveId = useCategoryStore((state) => state.activeId);
     return (
-        <div className={cn("mx-auto max-w-[1440px] flex justify-center sm:justify-normal sm:gap-6 bg-white " +
-            "text-sm sm:text-2xl", [className])}>
-            {
-                categories.map(({id, name}, index) => (
-                    <a className={cn("p-2 flex items-center hover:text-muted-foreground transition " +
-                        "text-nowrap",
-                        categoryActiveId === id && "rounded-2xl border-1 text-primary hover:text-primary"
-                    )} key={index}>
-                        <button>{name}</button>
-                    </a>
-                ))
-            }
+        <div className={cn("bg-white", [className])}>
+            <div className="mx-auto max-w-[1440px] flex justify-center sm:justify-normal sm:gap-6  text-sm sm:text-2xl">
+                {
+                    categories.map(({id, name}, index) => (
+                        <a className={cn("p-2 flex items-center hover:text-muted-foreground transition " +
+                            "text-nowrap",
+                            categoryActiveId === id && "rounded-2xl border-1 text-primary hover:text-primary"
+                        )} key={index}>
+                            <button>{name}</button>
+                        </a>
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
